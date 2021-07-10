0b05e4f616e7f9a1dd9c1e64abcb880ba725a905
Yamil Caceres <turcoaznar@gmail.com>
2021-07-10 20:17:21 -0300

    Estudio Contable

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards', {delay: 250 });
